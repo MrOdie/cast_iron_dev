021c312b34d5e1ff7e12ab4a820c9a2ce68f281b
MrOdie <sodonnell5214@gmail.com>
2022-10-05 22:21:28 -0400

    Added images and styles.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,30 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <h1>Home Page</h1>
-
+  <section>
+    <article>
+      <h2 class="h1">Cast Iron Dev</h2>
+      <p>Husband &mdash; Father &mdash; Developer &mdash; Cast Iron Fan</p>
+    </article>
+  </section>
 </template>
 
 <style scoped lang="scss">
+section {
+  height: 100vh;
+  width: 100vw;
+  display: grid;
+  grid-template-columns: 4fr 3fr 4fr;
+  grid-template-rows: 4fr 3fr 4fr;
+  background: url(/src/assets/images/home_large.jpg) no-repeat center/cover;
 
+  article {
+    text-align: center;
+    grid-column-start: 2;
+    grid-row-start: 2;
+    background-color: #eee;
+    border-radius: 0px 10px 0px 10px;
+    box-shadow: 0px 0px 0px 2px #333;
+  }
+}
 </style>
